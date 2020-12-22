c44e0f547403b3fb2238d1c77fb144f8b82075a2
hatrari <ham.atrari@gmail.com>
2020-12-22 18:40:06 +0100

    add Block model

---
+++ Models/Block.cs
@@ -0,0 +1,10 @@
+namespace TrackingBlocksAPI.Models
+{
+  public class Block
+  {
+    public string Date { get; set; }
+    public string Hour { get; set; }
+    public string Number { get; set; }
+    public string Color { get; set; }
+  }
+}
